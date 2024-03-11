e55cb836868ebf0383bfc7e4218c6130911e94f7
hizer00 <bastixvii@gmail.com>
2024-03-11 20:07:55 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
+  vite: {
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+  },
+});
