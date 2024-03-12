fd35991e54942a26b5e69b8e36e49c718b407eca
hizer00 <bastixvii@gmail.com>
2024-03-12 17:00:38 +0100

    Added skills section

---
+++ src/consts/skills.js
@@ -0,0 +1,44 @@
+const skills = [
+  {
+    name: "HTML",
+    image: "/img/tech/html-icon.png",
+    color: "#4B2A1D",
+  },
+  {
+    name: "CSS",
+    image: "/img/tech/css-icon.png",
+    color: "#15314D",
+  },
+  {
+    name: "JavaScript",
+    image: "/img/tech/js-icon.png",
+    color: "#47361E",
+  },
+  {
+    name: "React",
+    image: "/img/tech/react-icon.png",
+    color: "#15314D",
+  },
+  {
+    name: "Node.js",
+    image: "/img/tech/node-js-icon.png",
+    color: "#0e3310",
+  },
+  {
+    name: "Python",
+    image: "/img/tech/py-icon.png",
+    color: "#15314D",
+  },
+  {
+    name: "Rust",
+    image: "/img/tech/rust-icon.png",
+    color: "#4B2A1D",
+  },
+  {
+    name: "Git",
+    image: "/img/tech/git-icon.png",
+    color: "#4B2A1D",
+  },
+];
+
+export default skills;
