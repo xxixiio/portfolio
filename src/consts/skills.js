35f86a25a2c1282c91e5735f58cf69c5ad9dbe79
hizer00 <bastixvii@gmail.com>
2024-03-12 17:22:44 +0100

    Optimized skill icon images

--- src/consts/skills.js
+++ src/consts/skills.js
@@ -1,42 +1,42 @@
 const skills = [
   {
     name: "HTML",
-    image: "/img/tech/html-icon.png",
+    image: "/img/tech/html-icon.webp",
     color: "#4B2A1D",
   },
   {
     name: "CSS",
-    image: "/img/tech/css-icon.png",
+    image: "/img/tech/css-icon.webp",
     color: "#15314D",
   },
   {
     name: "JavaScript",
-    image: "/img/tech/js-icon.png",
+    image: "/img/tech/js-icon.webp",
     color: "#47361E",
   },
   {
     name: "React",
-    image: "/img/tech/react-icon.png",
+    image: "/img/tech/react-icon.webp",
     color: "#15314D",
   },
   {
     name: "Node.js",
-    image: "/img/tech/node-js-icon.png",
+    image: "/img/tech/node-js-icon.webp",
     color: "#0e3310",
   },
   {
     name: "Python",
-    image: "/img/tech/py-icon.png",
+    image: "/img/tech/py-icon.webp",
     color: "#15314D",
   },
   {
     name: "Rust",
-    image: "/img/tech/rust-icon.png",
+    image: "/img/tech/rust-icon.webp",
     color: "#4B2A1D",
   },
   {
     name: "Git",
-    image: "/img/tech/git-icon.png",
+    image: "/img/tech/git-icon.webp",
     color: "#4B2A1D",
   },
 ];
