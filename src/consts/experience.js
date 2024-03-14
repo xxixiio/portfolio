38dc84df0c5cb542e5232c667c40d41a3bbb10e0
Basti J <bastixvii@gmail.com>
2024-03-14 12:37:04 +0100

    Add experience section

---
+++ src/consts/experience.js
@@ -0,0 +1,12 @@
+const experience = [
+  {
+    name: "Freelance",
+    position: "Frontend Developer",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum iste sed nisi tenetur maiores similique!",
+    startDate: "Currently",
+    endDate: null,
+  },
+];
+
+export default experience;
