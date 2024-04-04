7a8007a3a3dc1f9570c2d9089d2521950f1d19cb
hizer00 <bastixvii@gmail.com>
2024-04-04 18:12:10 +0200

    Added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   build: {
-    inlineStylesheets: "always",
+    inlineStylesheets: "always"
   },
   vite: {
     ssr: {
-      noExternal: ["path-to-regexp"],
-    },
+      noExternal: ["path-to-regexp"]
+    }
   },
+  site: "https://xxixiio.tokyo",
+  integrations: [sitemap()]
 });
