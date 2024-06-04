a26e3c6700f860170a5955a9d9ff111659efbd64
hizer00 <bastixvii@gmail.com>
2024-06-05 00:07:02 +0200

    Added experience

--- src/consts/experience.js
+++ src/consts/experience.js
@@ -7,6 +7,14 @@ const experience = [
     startDate: "Currently",
     endDate: null,
   },
+  {
+    name: "IFIXIT NOW",
+    position: "IT Assistant",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum iste sed nisi tenetur maiores similique!",
+    startDate: "01/05/2024",
+    endDate: "30/05/2024",
+  },
 ];
 
 export default experience;
