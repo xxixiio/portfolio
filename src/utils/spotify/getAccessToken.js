844dfdf8dcb97896d394246a7daaa8eaa0fda307
hizer00 <bastixvii@gmail.com>
2024-06-16 22:45:21 +0200

    feat: added spotify connection

---
+++ src/utils/spotify/getAccessToken.js
@@ -0,0 +1,25 @@
+import querystring from "querystring";
+
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
+
+export const getAccessToken = async (
+  client_id,
+  client_secret,
+  refresh_token
+) => {
+  const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
+
+  const response = await fetch(TOKEN_ENDPOINT, {
+    method: "POST",
+    headers: {
+      Authorization: `Basic ${basic}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: querystring.stringify({
+      grant_type: "refresh_token",
+      refresh_token,
+    }),
+  });
+
+  return response.json();
+};
