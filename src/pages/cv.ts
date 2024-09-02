97fd157de4ce503491053fe15f01ef10672543cb
hizer00 <bastixvii@gmail.com>
2024-09-02 17:29:02 +0200

    fix: Removed CV

--- src/pages/cv.ts
+++
@@ -1,5 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = ({ redirect }) => {
-  return redirect('https://drive.google.com/file/d/16KYeciPzyNFK_a8nb1QTswqGtmu_SfdE/view', 307);
-}
--- src/pages/es/cv.ts
+++
@@ -1,5 +0,0 @@
-import type { APIRoute } from 'astro';
-
-export const GET: APIRoute = ({ redirect }) => {
-  return redirect('https://drive.google.com/file/d/1w3WZZYxIKSlDw90xxMhLUEwvVm7xf5IB/view', 307);
-}
