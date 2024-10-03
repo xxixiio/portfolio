67213cb0a1296e6ac5ad897c8933334787868e65
hizer00 <{ID}+{username}@users.noreply.github.com>
2024-10-04 00:47:49 +0200

    feat: added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,39 +3,34 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import astroI18next from "astro-i18next";
-
 import mdx from "@astrojs/mdx";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   build: {
-    inlineStylesheets: "always",
+    inlineStylesheets: "always"
   },
   vite: {
     ssr: {
-      noExternal: ["path-to-regexp"],
-    },
+      noExternal: ["path-to-regexp"]
+    }
   },
   site: "https://sebjf.dev",
-  integrations: [
-    sitemap(),
-    astroI18next(),
-    react({
-      include: ["**/react/*"],
-    }),
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: { theme: "rose-pine-moon" },
-    }),
-  ],
+  integrations: [sitemap(), astroI18next(), react({
+    include: ["**/react/*"]
+  }), mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "rose-pine-moon"
+    }
+  }), partytown()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
-    includeFiles: [
-      "./public/locales/en/translation.json",
-      "./public/locales/es/translation.json",
-    ],
-  }),
+    includeFiles: ["./public/locales/en/translation.json", "./public/locales/es/translation.json"]
+  })
 });
