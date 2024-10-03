0eed4b60e71a5f90d63686d9238c6ee9ab553d6d
hizer00 <{ID}+{username}@users.noreply.github.com>
2024-10-03 23:13:43 +0200

    fix: removed logs on nowPlaying.js

--- src/utils/spotify/nowPlaying.js
+++ src/utils/spotify/nowPlaying.js
@@ -7,47 +7,52 @@ const refresh_token = import.meta.env.SPOTIFY_REFRESH_TOKEN;
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 
 export const getNowPlaying = async () => {
-  try {
-    const { access_token } = await getAccessToken(
-      client_id,
-      client_secret,
-      refresh_token
-    );
+  // try {
+  const { access_token } = await getAccessToken(
+    client_id,
+    client_secret,
+    refresh_token
+  );
 
-    const response = await fetch(NOW_PLAYING_ENDPOINT, {
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-      },
-    });
+  const response = await fetch(NOW_PLAYING_ENDPOINT, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
 
-    if (response.status > 400) {
-      throw new Error("Unable to Fetch Song");
-    } else if (response.status === 204) {
-      throw new Error("Currently Not Playing");
-    }
-
-    const song = await response.json();
-    const albumImageUrl = !song.item.is_local ? song.item.album.images[0].url : null;
-    const artist = song.item.artists.map((artist) => artist.name).join(", ");
-    const isPlaying = song.is_playing;
-    const songUrl = !song.item.is_local ? song.item.external_urls.spotify : null;
-    const title = song.item.name;
-    const timePlayed = song.progress_ms;
-    const timeTotal = song.item.duration_ms;
-    const artistUrl = !song.item.is_local ? song.item.album.artists[0].external_urls.spotify : null;
-
-    return {
-      albumImageUrl,
-      artist,
-      isPlaying,
-      songUrl,
-      title,
-      timePlayed,
-      timeTotal,
-      artistUrl,
-    };
-  } catch (error) {
-    console.error("Error fetching currently playing song: ", error);
-    return error.message.toString();
+  if (response.status > 400) {
+    throw new Error("Unable to Fetch Song");
   }
+  // else if (response.status === 204) {
+  //   throw new Error("Currently Not Playing");
+  // }
+
+  const song = await response.json();
+  const albumImageUrl = !song.item.is_local
+    ? song.item.album.images[0].url
+    : null;
+  const artist = song.item.artists.map((artist) => artist.name).join(", ");
+  const isPlaying = song.is_playing;
+  const songUrl = !song.item.is_local ? song.item.external_urls.spotify : null;
+  const title = song.item.name;
+  const timePlayed = song.progress_ms;
+  const timeTotal = song.item.duration_ms;
+  const artistUrl = !song.item.is_local
+    ? song.item.album.artists[0].external_urls.spotify
+    : null;
+
+  return {
+    albumImageUrl,
+    artist,
+    isPlaying,
+    songUrl,
+    title,
+    timePlayed,
+    timeTotal,
+    artistUrl,
+  };
+  //   } catch (error) {
+  //     console.error("Error fetching currently playing song: ", error);
+  //     return error.message.toString();
+  //   }
 };
