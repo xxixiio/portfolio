34ed5f1786db2148c9266100e36940f78d159f18
hizer00 <108923627+xxixiio@users.noreply.github.com>
2025-03-11 00:25:38 +0100

    feat: man, i cant deal with colouring svg dynamiclly using map in astro :( (starting skills rework)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,33 +4,43 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 import astroI18next from "astro-i18next";
 import mdx from "@astrojs/mdx";
+import svgLoader from "vite-svg-loader";
 
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   build: {
-    inlineStylesheets: "always"
+    inlineStylesheets: "always",
   },
   vite: {
     ssr: {
-      noExternal: ["path-to-regexp"]
-    }
+      noExternal: ["path-to-regexp"],
+    },
   },
   site: "https://sebjf.dev",
-  integrations: [sitemap(), astroI18next(), react({
-    include: ["**/react/*"]
-  }), mdx({
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "rose-pine-moon"
-    }
-  }), partytown()],
+  integrations: [
+    sitemap(),
+    astroI18next(),
+    react({
+      include: ["**/react/*"],
+    }),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: {
+        theme: "rose-pine-moon",
+      },
+    }),
+    partytown(),
+  ],
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true
+      enabled: true,
     },
-    includeFiles: ["./public/locales/en/translation.json", "./public/locales/es/translation.json"]
-  })
+    includeFiles: [
+      "./public/locales/en/translation.json",
+      "./public/locales/es/translation.json",
+    ],
+  }),
 });
---
+++ src/consts/skills.new.js
@@ -0,0 +1,52 @@
+const skills = [
+    {
+        "name": "HTML 5",
+        "icon": "html.astro",
+        "color": "",
+        "description": ""
+    },
+    {
+        "name": "CSS",
+        "icon": "css.astro",
+        "color": "",
+        "description": ""
+    },
+    {
+        "name": "JavaScript",
+        "icon": "javascript.astro",
+        "color": "",
+        "description": ""
+    },
+    {
+        "name": "Astro",
+        "icon": "astro.astro",
+        "color": "",
+        "description": ""
+    },
+    {
+        "name": "React.js",
+        "icon": "react.astro",
+        "color": "",
+        "description": ""
+    },
+    {
+        "name": "express",
+        "icon": "express.astro",
+        "color": "",
+        "description": ""
+    },
+    {
+        "name": "MongoDB",
+        "icon": "mongodb.astro",
+        "color": "",
+        "description": ""
+    },
+    {
+        "name": "Figma",
+        "icon": "figma.astro",
+        "color": "",
+        "description": ""
+    },
+]
+
+export default skills;
