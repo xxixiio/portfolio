93f4ad5029d63a74824960760812fcaa9dbe514d
hizer00 <108923627+xxixiio@users.noreply.github.com>
2025-03-12 00:04:26 +0100

    feat: add dynamic SVG component and update SVG fill colors

--- src/consts/skills.new.js
+++ src/consts/skills.new.js
@@ -1,52 +1,52 @@
 const skills = [
     {
         "name": "HTML 5",
-        "icon": "html.astro",
-        "color": "",
-        "description": ""
+        "icon": "/img/technologies/html.svg",
+        "color": "#E34F26",
+        "description": "Markup language for structuring web content."
     },
     {
         "name": "CSS",
-        "icon": "css.astro",
-        "color": "",
-        "description": ""
+        "icon": "/img/technologies/css.svg",
+        "color": "#1572B6",
+        "description": "Stylesheet language for designing web pages."
     },
     {
         "name": "JavaScript",
-        "icon": "javascript.astro",
-        "color": "",
-        "description": ""
+        "icon": "/img/technologies/javascript.svg",
+        "color": "#F7DF1E",
+        "description": "Programming language for dynamic web applications."
     },
     {
         "name": "Astro",
-        "icon": "astro.astro",
-        "color": "",
-        "description": ""
+        "icon": "/img/technologies/astro.svg",
+        "color": "#FF5D01",
+        "description": "Static site generator optimized for performance."
     },
     {
         "name": "React.js",
-        "icon": "react.astro",
-        "color": "",
-        "description": ""
+        "icon": "/img/technologies/react.svg",
+        "color": "#61DAFB",
+        "description": "Library for building interactive UI components."
     },
     {
-        "name": "express",
-        "icon": "express.astro",
-        "color": "",
-        "description": ""
+        "name": "Express",
+        "icon": "/img/technologies/express.svg",
+        "color": "#ffffff",
+        "description": "Minimalist web framework for Node.js."
     },
     {
         "name": "MongoDB",
-        "icon": "mongodb.astro",
-        "color": "",
-        "description": ""
+        "icon": "/img/technologies/mongodb.svg",
+        "color": "#47A248",
+        "description": "NoSQL database for scalable applications."
     },
     {
         "name": "Figma",
-        "icon": "figma.astro",
-        "color": "",
-        "description": ""
-    },
-]
+        "icon": "/img/technologies/figma.svg",
+        "color": "#F24E1E",
+        "description": "Collaborative design tool for UI/UX projects."
+    }
+];
 
 export default skills;
