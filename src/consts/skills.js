e22cf7afaddfb7d7d1a1e6c6b26eab136f0ac964
seb <108923627+xxixiio@users.noreply.github.com>
2026-04-13 16:47:05 +0200

    chore: remove skills.old and add two more skills

--- src/consts/skills.js
+++ src/consts/skills.js
@@ -1,65 +1,70 @@
 const skills = [
-  {
-    name: "HTML",
-    image: "/img/tech/html-icon.webp",
-    color: "red",
-  },
-  {
-    name: "CSS",
-    image: "/img/tech/css-icon.webp",
-    color: "blue",
-  },
-  {
-    name: "JavaScript",
-    image: "/img/tech/js-icon.webp",
-    color: "yellow",
-  },
-  {
-    name: "React",
-    image: "/img/tech/react-icon.webp",
-    color: "blue",
-  },
-  {
-    name: "Astro",
-    image: "/img/tech/astro-icon.webp",
-    color: "purple",
-  },
-  {
-    name: "Node.js",
-    image: "/img/tech/node-js-icon.webp",
-    color: "green",
-  },
-  {
-    name: "MongoDB",
-    image: "/img/tech/mongo-icon.webp",
-    color: "green",
-  },
-  {
-    name: "Python",
-    image: "/img/tech/py-icon.webp",
-    color: "blue",
-  },
-  {
-    name: "Git",
-    image: "/img/tech/git-icon.webp",
-    color: "red",
-  },
-  {
-    name: "WordPress",
-    image: "/img/tech/wordpress-icon.webp",
-    color: "blue",
-  },
-  {
-    name: "Linux",
-    image: "/img/tech/linux-icon.webp",
-    color: "yellow",
-  },
-  // {
-  //   name: "Penpot",
-  //   image: "/img/tech/penpot-icon.webp",
-  //   color: "blue",
-  // },
-  
+    {
+        "name": "HTML5",
+        "icon": "/img/technologies/html.svg",
+        "color": "#E34F26",
+        "description": "Markup language for structuring web content."
+    },
+    {
+        "name": "CSS3",
+        "icon": "/img/technologies/css.svg",
+        "color": "#1572B6",
+        "description": "Stylesheet language for designing web pages."
+    },
+    {
+        "name": "JavaScript",
+        "icon": "/img/technologies/javascript.svg",
+        "color": "#F7DF1E",
+        "description": "Programming language for dynamic web applications."
+    },
+    {
+        "name": "Astro",
+        "icon": "/img/technologies/astro.svg",
+        "color": "#FF5D01",
+        "description": "Static site generator optimized for performance."
+    },
+    {
+        "name": "React.js",
+        "icon": "/img/technologies/react.svg",
+        "color": "#61DAFB",
+        "description": "Library for building interactive UI components."
+    },
+    {
+        "name": "Node.js",
+        "icon": "/img/technologies/nodejs.svg",
+        "color": "#539E43",
+        "description": "/"
+    },
+    {
+        "name": "Express",
+        "icon": "/img/technologies/express.svg",
+        "color": "#ffffff",
+        "description": "Minimalist web framework for Node.js."
+    },
+    {
+        "name": "MongoDB",
+        "icon": "/img/technologies/mongodb.svg",
+        "color": "#47A248",
+        "description": "NoSQL database for scalable applications."
+    },
+    {
+        "name": "MySQL",
+        "icon": "/img/technologies/mysql.svg",
+        "color": "#5D87A1",
+        "description": "/"
+    },
+    {
+        "name": "Figma",
+        "icon": "/img/technologies/figma.svg",
+        "color": "#F24E1E",
+        "description": "Collaborative design tool for UI/UX projects."
+    },
+    {
+        "name": "n8n",
+        "icon": "/img/technologies/n8n.svg",
+        "color": "#E94B71",
+        "description": "Lorem."
+    }
 ];
 
 export default skills;
